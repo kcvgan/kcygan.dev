d4ad0e641ed10ed31e7656d2b7e097b31f6960fc
Kacper Cygan <kcygan54@gmail.com>
2019-03-26 06:54:15 +0100

    Added gh pages branch.

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,11 +1,11 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Starter Blog`,
-    author: `Kyle Mathews`,
-    description: `A starter blog demonstrating what Gatsby can do.`,
-    siteUrl: `https://gatsby-starter-blog-demo.netlify.com/`,
+    title: `kcygan.dev`,
+    author: `Kacper Cygan`,
+    description: `My personal space.`,
+    siteUrl: `https://kcygan.dev/`,
     social: {
-      twitter: `kylemathews`,
+      twitter: `kcygan`,
     },
   },
   plugins: [
